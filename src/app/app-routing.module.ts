1e60f4f8fceefb2a58f1fb2162c3ff77d04a95d6
benoitlehnig <benoit.lehnig@gmail.com>
2021-03-25 21:29:56 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,14 +40,7 @@ const routes: Routes = [
   path: 'piscinistPortal',
   loadChildren: () => import('./piscinist-portal/piscinist-portal.module').then( m => m.PiscinistPortalPageModule),
   canActivate: [AngularFireAuthGuard], data: { authGuardPipe: adminEmployeeOnly },
-},
-{
-  path: 'common-pages',
-  loadChildren: () => import('./common-pages/common-pages.module').then( m => m.CommonPagesPageModule)
-},  {
-    path: 'swimming-pool-read-only',
-    loadChildren: () => import('./common/swimming-pool-read-only/swimming-pool-read-only.module').then( m => m.SwimmingPoolReadOnlyPageModule)
-  }
+}
 
 ];
 
